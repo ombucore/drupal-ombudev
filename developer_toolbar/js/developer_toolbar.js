44cb22fcc0b2d41a2fbfdf6e408bf602b827b49c
Jon Duell <jon@jonduell.com>
2015-08-21 15:11:25 -0700

    Adds more robust database query collector

--- developer_toolbar/js/developer_toolbar.js
+++ developer_toolbar/js/developer_toolbar.js
@@ -1,25 +1,114 @@
-if (typeof(DevelDebugBar) == 'undefined') {
+if (typeof(DeveloperToolbar) == 'undefined') {
   // namespace
-  var DevelDebugBar = {};
-  DevelDebugBar.Widgets = {};
+  var DeveloperToolbar = {};
+  DeveloperToolbar.Widgets = {};
 
 }
 
 (function($, PhpDebugBar) {
 
-  DevelDebugBar.Widgets.MarkupWidget = PhpDebugBar.Widget.extend({
+  var csscls = PhpDebugBar.utils.makecsscls('phpdebugbar-widgets-');
 
-  className: 'debug-bar-markup-widget',
+  DeveloperToolbar.Widgets.DatabaseWidget = PhpDebugBar.Widget.extend({
 
-  render: function() {
+    className: csscls('database'),
 
-    this.bindAttr('data', function(data) {
-      this.$el.html(data);
+    onFilterClick: function(el) {
+      $(el).toggleClass(csscls('excluded'));
+
+      var excludedLabels = [];
+      this.$toolbar.find(csscls('.filter') + csscls('.excluded')).each(function() {
+        excludedLabels.push(this.rel);
+      });
+
+      this.$list.$el.find("li[connection=" + $(el).attr("rel") + "]").toggle();
+
+      this.set('exclude', excludedLabels);
+    },
+
+    render: function() {
+      this.$status = $('<div />').addClass(csscls('status')).appendTo(this.$el);
+
+      this.$toolbar = $('<div></div>').addClass(csscls('toolbar')).appendTo(this.$el);
+
+      var filters = [], self = this;
+
+      this.$list = new PhpDebugBar.Widgets.ListWidget({ itemRenderer: function(li, stmt) {
+        $('<code />').addClass(csscls('sql')).html(PhpDebugBar.Widgets.highlight(stmt.sql, 'sql')).appendTo(li);
+        if (stmt.duration) {
+          $('<span title="Duration" />').addClass(csscls('duration')).text(stmt.duration).appendTo(li);
+        }
+        if (stmt.connection) {
+          $('<span title="Connection" />').addClass(csscls('database')).text(stmt.connection).appendTo(li);
+          li.attr("connection",stmt.connection);
+          if ( $.inArray(stmt.connection, filters) == -1 ) {
+            filters.push(stmt.connection);
+            $('<a href="javascript:" />')
+              .addClass(csscls('filter'))
+              .text(stmt.connection)
+              .attr('rel', stmt.connection)
+              .on('click', function() { self.onFilterClick(this); })
+              .appendTo(self.$toolbar);
+            if (filters.length>1) {
+              self.$toolbar.show();
+              self.$list.$el.css("margin-bottom","20px");
+            }
+          }
+        }
+        if (stmt.params && !$.isEmptyObject(stmt.params)) {
+          var table = $('<table><tr><th colspan="2">Params</th></tr></table>').addClass(csscls('params')).appendTo(li);
+          for (var key in stmt.params) {
+            if (typeof stmt.params[key] !== 'function') {
+              table.append('<tr><td class="' + csscls('name') + '">' + key + '</td><td class="' + csscls('value') +
+                  '">' + stmt.params[key] + '</td></tr>');
+            }
+          }
+          li.css('cursor', 'pointer').click(function() {
+            if (table.is(':visible')) {
+              table.hide();
+            } else {
+              table.show();
+            }
+          });
+        }
+      }});
+      this.$list.$el.appendTo(this.$el);
+
+      this.bindAttr('data', function(data) {
+        this.$list.set('data', data.statements);
+        this.$status.empty();
+
+        // Search for duplicate statements.
+        for (var sql = {}, duplicate = 0, i = 0; i < data.statements.length; i++) {
+          var stmt = data.statements[i].sql;
+          if (data.statements[i].params && !$.isEmptyObject(data.statements[i].params)) {
+            stmt += ' {' + $.param(data.statements[i].params, false) + '}';
+            }
+            sql[stmt] = sql[stmt] || { keys: [] };
+            sql[stmt].keys.push(i);
+          }
+          // Add classes to all duplicate SQL statements.
+          for (var stmt in sql) {
+            if (sql[stmt].keys.length > 1) {
+              duplicate++;
+              for (var i = 0; i < sql[stmt].keys.length; i++) {
+                this.$list.$el.find('.' + csscls('list-item')).eq(sql[stmt].keys[i])
+                  .addClass(csscls('sql-duplicate')).addClass(csscls('sql-duplicate-'+duplicate));
+              }
+            }
+          }
+
+          var t = $('<span />').text(data.total_statements + " statements were executed").appendTo(this.$status);
+          if (duplicate) {
+            t.append(", " + duplicate + " of which were duplicated");
+          }
+          if (data.total_duration) {
+            this.$status.append($('<span title="Accumulated duration" />').addClass(csscls('duration')).text(data.total_duration));
+          }
+        });
+      }
+
     });
-
-  }
-
-});
 
 })(jQuery, PhpDebugBar);
 
--- developer_toolbar/lib/DeveloperToolbar/DatabaseCollector.php
+++ developer_toolbar/lib/DeveloperToolbar/DatabaseCollector.php
@@ -10,13 +10,58 @@ use DebugBar\DataCollector\Renderable;
  */
 class DatabaseCollector extends DataCollector implements Renderable {
   /**
+   * Flag to format SQL with parameters.
+   */
+  protected $formatWithParameters = FALSE;
+
+  /**
    * {@inheritDoc}
    */
   public function collect() {
+    $statements = array();
+    $total_duration = 0;
+
     $queries = \Database::getLog('developer_toolbar', 'default');
+    foreach ($queries as $query) {
+      $total_duration += $query['time'];
+      $statements[] = array(
+        'sql' => $this->formatWithParameters ? $this->formatSql($query) : $query['query'],
+        'params' => (object) $query['args'],
+        'duration' => $this->formatDuration($query['time']),
+        'caller' => $query['caller'],
+        'connection' => $query['target'],
+      );
+    }
+
     return array(
-      'database_str' => count($queries),
+      'total_statements' => count($statements),
+      'statements' => $statements,
+      'total_duration' => $this->formatDuration($total_duration),
     );
+  }
+
+  /**
+   * Helper function to format sql statements with parameters.
+   */
+  protected function formatSql($query, $quotationChar = '<>') {
+    if (($l = strlen($quotationChar)) > 1) {
+      $quoteLeft = substr($quotationChar, 0, $l / 2);
+      $quoteRight = substr($quotationChar, $l / 2);
+    } else {
+      $quoteLeft = $quoteRight = $quotationChar;
+    }
+
+    $sql = $query['query'];
+    foreach ($query['args'] as $k => $v) {
+      $v = "$quoteLeft$v$quoteRight";
+      if (!is_numeric($k)) {
+        $sql = str_replace($k, $v, $sql);
+      } else {
+        $p = strpos($sql, '?');
+        $sql = substr($sql, 0, $p) . $v. substr($sql, $p + 1);
+      }
+    }
+    return $sql;
   }
 
   /**
@@ -33,11 +78,14 @@ class DatabaseCollector extends DataCollector implements Renderable {
     return array(
       'database' => array(
         'icon' => 'database',
-        'tooltip' => 'Database queries',
-        'map' => 'database.database_str',
-        'default' => 'null'
+        'widget' => 'DeveloperToolbar.Widgets.DatabaseWidget',
+        'map' => 'database',
+        'default' => '[]'
+      ),
+      'database:badge' => array(
+        'map' => 'database.total_statements',
+        'default' => 0,
       )
     );
   }
-
 }
